97a1006f0d8e771f278e2bbeb7ad12c90ca72861
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2016-11-25 15:38:05 -0200

    Add bookmarklet link at control panel

--- src/collective/publishthis/browser/controlpanel.py
+++ src/collective/publishthis/browser/controlpanel.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from collective.publishthis import _
 from collective.publishthis.interfaces import IPublishThisSettings
+from plone import api
 from plone.app.registry.browser import controlpanel
+from plone.z3cform import layout
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class PackageSettingsEditForm(controlpanel.RegistryEditForm):
@@ -11,6 +14,36 @@ class PackageSettingsEditForm(controlpanel.RegistryEditForm):
     description = _(u'Here you can modify the settings for collective.publishthis.')
 
 
-class PackageSettingsControlPanel(controlpanel.ControlPanelFormWrapper):
+class PackageSettingsControlPanel(layout.FormWrapper):
 
     form = PackageSettingsEditForm
+    index = ViewPageTemplateFile('controlpanel_layout.pt')
+
+    @property
+    def control_panel_url(self):
+        portal = api.portal.get()
+        return u'{0}/@@overview-controlpanel'.format(portal.absolute_url())
+
+    @property
+    def bookmarklet(self):
+        return (
+            'javascript:(function(){{'
+            'window.open(\'{0}?'
+            'url=\'+location.href+\'&'
+            'title=\'+document.title+\'&'
+            'text=\'+window.getSelection(),'
+            '\'_blank\','
+            '\'location,resizable,scrollbars,'
+            'width=\'+window.outerWidth+\','
+            'height=\'+window.outerHeight);}})()'
+        ).format(self.url)
+
+    @property
+    def url(self):
+        portal = api.portal.get()
+        return u'{0}/@@publish-this'.format(portal.absolute_url())
+
+    @property
+    def title(self):
+        portal = api.portal.get()
+        return portal.Title()
